5426d0f7fa062f83c9c2b2eacbd4d178aeda0bf8
k-abhishek <abhishek@deepcompute.com>
2019-03-14 21:37:23 +0530

    cors issue in deployment

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@ const config = {
     appName: 'Strings.ai Giflix',
     homepage: '/',
     primaryColor: '#e50914',
-    url: 'http://api.giphy.com/v1/gifs'
+    url: 'https://api.giphy.com/v1/gifs'
   }
   //Url: typeof window === 'undefined' || window.location.hostname === 'localhost' ? '' : '//' + window.location.hostname
 export default config
